a13dce5300e85a9d294cb185de90dc569b82d6a5
Nikola Nemes <nikolanemzi@gmail.com>
2020-02-14 12:55:15 +0100

    Cleaning

--- main.py
+++
@@ -1,9 +0,0 @@
-from ahk import AHK
-import time
-
-ahk = AHK(executable_path="F:\\Program Files\\AutoHotkey\\AutoHotkey.exe")
-
-while True:
-    ahk.mouse_move(100, 0, speed=1, relative=True)  # Moves the mouse reletave to the current position
-    ahk.click()  # Click the primary mouse button
-    time.sleep(2)
--- mainPyInput.py
+++
@@ -1,9 +0,0 @@
-from pynput.mouse import Button, Controller
-import time
-
-mouse = Controller()
-
-while True:
-    time.sleep(2)
-    mouse.move(400, 0)
-    mouse.click(Button.left, 1)
